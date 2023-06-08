30d0c11c51cd9932c7567446b01c81f0abadff51
Akash kumaraguru <131514244+akashkumaraguru@users.noreply.github.com>
2023-06-09 00:32:51 +0530

    Add files via upload

---
+++ pass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class pass{
+    public static void main(String[] args) {
+        int passmark=35,a;
+      Scanner s =new Scanner(System.in);
+        System.out.println("enter your mark");
+        a=s.nextInt();
+        if (a>=passmark){
+            System.out.println("you're pass mameeaa");
+        }
+        else{
+            System.out.println("padiii da paraamaa");
+        }
+
+
+
+    }
+}
