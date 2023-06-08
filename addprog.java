d52d6d191343923a719ad4d11ab45c034292fb5a
Akash kumaraguru <131514244+akashkumaraguru@users.noreply.github.com>
2023-06-09 00:17:41 +0530

    Add files via upload

---
+++ addprog.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class addprog{
+    public static void main(String[] args){
+        System.out.println("enter a number : ");
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=a+b;
+        System.out.println(c );
+
+    }
+
+}
